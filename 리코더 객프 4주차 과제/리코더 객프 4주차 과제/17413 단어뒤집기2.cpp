1c9e4427d95da02c3bb06e5b99b4ec96dfa6f0de
WonJung <won_jung0612@naver.com>
2026-02-06 16:21:17 +0900

    17413번 해결

--- 리코더 객프 4주차 과제/리코더 객프 4주차 과제/17413 단어뒤집기2.cpp
+++ 리코더 객프 4주차 과제/리코더 객프 4주차 과제/17413 단어뒤집기2.cpp
@@ -1,11 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdio.h>
 #include <string.h>
 using namespace std;
 
+char* reverse(char* word) {
+	int len = strlen(word);
+	char* new_word = new char[len + 1];
+	for (int i = 0; i < len; i++) {
+		new_word[i] = word[len - 1 - i];
+	}
+	new_word[len] = '\0';
+	return new_word;
+}
+
 int main() {
-	char s[100000];
-	gets_s(s, 100000);
+	char s[100001];
+	fgets(s, 100001, stdin);
 
 	int len = strlen(s);
 	char* word = new char[len];
@@ -15,13 +26,33 @@ int main() {
 		if (s[i] == '<') {
 			for (int j = i, start=0; ; j++, start++) {
 				word[start] = s[j];
-				if (s[j] != '>') {
+				if (s[j] == '>') {
 					word[start + 1] = '\0';
 					i += start;
+					break;
 				}
 			}
 			strcat(new_s, word);
-			continue;
+		}
+		else if(s[i] == ' ')
+			strcat(new_s, " ");
+		
+		else {
+			for (int j = i, start = 0; ; j++, start++) {
+				if (s[j] == '\n') {
+					word[start] = '\0';
+					i += start;
+					break;
+				}
+				else if (s[j] == ' ' || s[j] == '\0' || s[j] == '<') {
+					word[start] = '\0';
+					i += start-1;
+					break;
+				}
+				word[start] = s[j];
+			}
+			char* rev_word = reverse(word);
+			strcat(new_s, rev_word);
 		}
 	}
 	cout << new_s;
